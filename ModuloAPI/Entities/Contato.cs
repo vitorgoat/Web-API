80c95f66d2fa6abef99a151a58c871c74f5dcd48
Vitor <vitor.brito.braga@hotmail.com>
2024-09-30 14:35:49 -0300

    Web-API

---
+++ ModuloAPI/Entities/Contato.cs
@@ -0,0 +1,10 @@
+﻿namespace ModuloAPI.Entities
+{
+    public class Contato
+    {
+        public string Nome { get; set; }
+        public int Id { get; set; }
+        public string Telefone { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
---
+++ ModuloAPI/Migrations/20240927183239_CriacaoTabelaContato.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ModuloAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoTabelaContato : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "contatos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ativo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_contatos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "contatos");
+        }
+    }
+}
